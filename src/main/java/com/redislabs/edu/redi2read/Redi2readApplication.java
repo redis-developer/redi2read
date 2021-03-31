8b58e7c89f4fcce1d88886b52043d13979a18727
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-03-31 15:38:31 -0700

    feature: spring initializr generated app

---
+++ src/main/java/com/redislabs/edu/redi2read/Redi2readApplication.java
@@ -0,0 +1,13 @@
+package com.redislabs.edu.redi2read;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redi2readApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Redi2readApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redislabs/edu/redi2read/Redi2readApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redislabs.edu.redi2read;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redi2readApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
