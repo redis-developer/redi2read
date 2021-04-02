3b2ba1d378b9f151f94a4d2024a0159c7d4c64b6
Brian Sam-Bodden <bsbodden@integrallis.com>
2021-04-02 01:26:04 -0700

    config: configure RedisTemplate bean

--- src/main/java/com/redislabs/edu/redi2read/Redi2readApplication.java
+++ src/main/java/com/redislabs/edu/redi2read/Redi2readApplication.java
@@ -2,12 +2,23 @@ package com.redislabs.edu.redi2read;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class Redi2readApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Redi2readApplication.class, args);
-	}
+  @Bean
+  public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<?, ?> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+
+    return template;
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(Redi2readApplication.class, args);
+  }
 
 }
