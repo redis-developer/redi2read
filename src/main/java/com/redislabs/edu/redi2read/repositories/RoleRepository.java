011fa6aab6eddcccc47f09e4f7e2736a7f2dcae7
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-04-02 21:17:13 -0700

    feature: add RoleRepository

---
+++ src/main/java/com/redislabs/edu/redi2read/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.redislabs.edu.redi2read.repositories;
+
+import com.redislabs.edu.redi2read.models.Role;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, String> {
+}
