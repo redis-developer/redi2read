709dfbae60c96496e5799a87949c9bf541b82e81
Brian Sam-Bodden <brian.sam-bodden@redislabs.com>
2021-04-03 15:11:32 -0700

    feature: add find user by email to UserController

--- src/main/java/com/redislabs/edu/redi2read/controllers/UserController.java
+++ src/main/java/com/redislabs/edu/redi2read/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.redislabs.edu.redi2read.controllers;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import com.redislabs.edu.redi2read.models.User;
 import com.redislabs.edu.redi2read.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,7 +21,12 @@ public class UserController {
   private UserRepository userRepository;
 
   @GetMapping
-  public Iterable<User> all() {
-    return userRepository.findAll();
+  public Iterable<User> all(@RequestParam(defaultValue = "") String email) {
+    if (email.isEmpty()) {
+      return userRepository.findAll();
+    } else {
+      Optional<User> user = Optional.ofNullable(userRepository.findFirstByEmail(email));
+      return user.isPresent() ? List.of(user.get()) : Collections.emptyList();
+    }
   }
 }
